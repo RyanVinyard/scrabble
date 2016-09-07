5bc1b17bf82bffac8a3074650cc0a37ad23333a6
Ryan Vinyard <rav.ryanvinyard@gmail.com>
2016-09-07 13:33:49 -0700

    calculate correct letter score for any single letter

--- src/main/java/Scrabble.java
+++ src/main/java/Scrabble.java
@@ -9,7 +9,17 @@ public class Scrabble {
     result = 1;
   } else if (letter.equals("d")) {
     result = 2;
+  } else if (letter.equals("b")) {
+    result = 3;
+  } else if (letter.equals("f")) {
+    result = 4;
+  } else if (letter.equals("k")) {
+    result = 5;
+  } else if (letter.equals("j")) {
+    result = 8;
+  } else if (letter.equals("q")) {
+    result = 10;
   }
-    return result;
+  return result;
   }
 }
--- src/test/java/ScrabbleTest.java
+++ src/test/java/ScrabbleTest.java
@@ -25,6 +25,34 @@ public class ScrabbleTest {
     Integer expected = 3;
     assertEquals(expected, testScrabble.calculateScore("b"));
   }
+
+  @Test
+  public void calculateScore_returnsScoreForSingleLetter_4() {
+    Scrabble testScrabble = new Scrabble();
+    Integer expected = 4;
+    assertEquals(expected, testScrabble.calculateScore("f"));
+  }
+
+  @Test
+  public void calculateScore_returnsScoreForLetter_K() {
+    Scrabble testScrabble = new Scrabble();
+    Integer expected = 5;
+    assertEquals(expected, testScrabble.calculateScore("k"));
+  }
+
+  @Test
+  public void calculateScore_returnsScoreForSingleLetter_8() {
+    Scrabble testScrabble = new Scrabble();
+    Integer expected = 8;
+    assertEquals(expected, testScrabble.calculateScore("j"));
+  }
+
+  @Test
+  public void calculateScore_returnsScoreForSingleLetter_10() {
+    Scrabble testScrabble = new Scrabble();
+    Integer expected = 10;
+    assertEquals(expected, testScrabble.calculateScore("q"));
+  }
 }
 
 //I was just about to test and make sure that third test up there fails (navigate to desktop/scrabble and run gradle test) and then code in Scrabble.java for it. It'll just add on to the if loop for now. Eventually use a for loop probably with charAt() to push in this letter values.
